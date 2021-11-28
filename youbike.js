ee71b5334b05ef251edc6a1965ed6d08c26b1aeb
roywangroytw <roywangroy.tw@gmail.com>
2021-11-28 16:00:42 +0800

    adding files and started working on html & JS

---
+++ youbike.js
@@ -0,0 +1,47 @@
+$(document).ready(function (){
+
+  const searchbtn = document.querySelector("#searchbtn");
+  const search_input = document.querySelector("#search_input");
+
+  searchbtn.addEventListener('click', (term) => {
+    
+    const inputValue = search_input.value.trim();
+
+    // 如果input有valide 值，才會call api
+    if (inputValue !== "") {
+
+      $.ajax({
+        url: 'https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json',
+        dataType: 'json',
+        success: function(data) {
+
+          let count = 0
+
+          data.forEach((item) => {
+
+            if (item.sarea === inputValue) {
+              
+            }
+
+          })
+    
+          // let count = 0;
+    
+          // data.forEach((item) => {
+          //   if (item.sarea === "中山區") {
+          //       count += 1;
+          //     }
+          //   })
+    
+          // console.log(count);
+    
+        }
+      
+    });
+
+    }
+
+  })
+
+
+})
