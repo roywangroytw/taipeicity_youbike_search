15b573e52fb3535a45ba53d8dd39e9778867598b
roywangroytw <roywangroy.tw@gmail.com>
2021-12-01 22:21:15 +0800

    finished refactoring

--- youbike.js
+++ youbike.js
@@ -6,6 +6,7 @@ $(document).ready(function () {
   const resultContainer = document.querySelector(".row");
   const footer = document.querySelector("footer");
 
+  // Event 1: When searched
   searchbtn.addEventListener("click", (term) => {
     const inputValueDisc = searchInputDisc.value;
     const inputValueTxt = searchInputTxt.value.trim();
@@ -13,13 +14,86 @@ $(document).ready(function () {
     const youBikeData = callAPI(inputValueDisc,inputValueTxt);
 
     // 2. 如果data沒有result, 就由某個function處理(長element的方式，不要alert)
-    if (youBikeData.length === 0) noResult();
+    if (youBikeData.length === 0) return noResult();
 
     // 3. 如果有data, 就交由另外一個function去處理渲染資訊
-    if (youBikeData) return 
+    if (youBikeData) return showResult(youBikeData)
 
+  });
 
-  })
+  //  Event 2: clear the search input ＆ search result
+  clearbtn.addEventListener("click", (term) => {
+    searchInputDisc.value = "";
+    searchInputTxt.value = "";
+    resultContainer.innerHTML = "";
+    footer.className = "footer";
+  });
+
+  function showResult(youBikeData){
+    youBikeData.forEach((item) => {
+      const { sna: stationName, tot: totalSpots, sbi: freeSlots, sarea: district, ar: address } = item;
+      const resultItem = document.createElement("div");
+      const resultItemLink = document.createElement("a");
+      resultItemLink.className = "item_link";
+      resultItemLink.href = `https://www.google.com/maps/?q=${address}`;
+      resultItemLink.target = "_blank";
+
+      const resultItemContent = document.createElement("div");
+      resultItem.className = "col-12 col-md-6 col-lg-4";
+      if (stationName.match("捷運")) {
+        resultItemContent.className = "content-box mrt";
+      } else {
+        resultItemContent.className = "content-box";
+      }
+
+      const resultItemInfo = document.createElement("div");
+      resultItemInfo.className = "info";
+      const resultItemInfoName = document.createElement("h2");
+      const resultItemInfoDistrict = document.createElement("p");
+      const resultItemInfoAddress = document.createElement("p");
+
+      const resultItemData = document.createElement("div");
+      resultItemData.className = "data";
+      const resultItemDataTotal = document.createElement("p");
+      const resultItemDataAva = document.createElement("p");
+      const resultItemDataAll = document.createElement("p");
+      const resultItemDataFree = document.createElement("p");
+
+      if (freeSlots === 0) {
+        resultItemDataFree.className = "no_spot";
+      }
+
+      resultItemInfoName.innerText = stationName.replace("YouBike2.0_", "").replace("捷運", "");
+      resultItemInfoDistrict.innerText = district;
+      resultItemInfoAddress.innerText = address;
+      resultItemDataTotal.innerText = "Total";
+      resultItemDataAva.innerText = "Available";
+      resultItemDataAll.innerText = totalSpots;
+      resultItemDataFree.innerText = freeSlots;
+
+      resultContainer.appendChild(resultItem);
+      resultItem.appendChild(resultItemContent);
+      resultItemContent.append(
+        resultItemInfo,
+        resultItemData,
+        resultItemLink,
+      );
+      resultItemInfo.append(
+        resultItemInfoName,
+        resultItemInfoDistrict,
+        resultItemInfoAddress
+      );
+      resultItemData.append(
+        resultItemDataTotal,
+        resultItemDataAva,
+        resultItemDataAll,
+        resultItemDataFree
+      );
+
+      footer.className = "footer_w_content";
+
+    })
+  }
 
   function noResult(){
     const noResultcontainer = document.createElement("div");
@@ -68,97 +142,7 @@ $(document).ready(function () {
 })
 
 
-
-
-
-// 把要實作的部份抽出來變成function
-// 用early return ( 後面還可以接function or alert )
-// 裡面的code就會有使用兩個function (一個是show alert / 一個是create element)
-  
-
-    // 如果input有valide 值，才會call api
-
-
-            //   console.log(item);
-            //   const resultItem = document.createElement("div");
-            //   const resultItemLink = document.createElement("a");
-            //   resultItemLink.className = "item_link";
-            //   resultItemLink.href = `https://www.google.com/maps/?q=${address}`;
-            //   resultItemLink.target = "_blank";
-
-            //   const resultItemContent = document.createElement("div");
-            //   resultItem.className = "col-12 col-md-6 col-lg-4";
-            //   if (stationName.match("捷運")) {
-            //     resultItemContent.className = "content-box mrt";
-            //   } else {
-            //     resultItemContent.className = "content-box";
-            //   }
-
-            //   const resultItemInfo = document.createElement("div");
-            //   resultItemInfo.className = "info";
-            //   const resultItemInfoName = document.createElement("h2");
-            //   const resultItemInfoDistrict = document.createElement("p");
-            //   const resultItemInfoAddress = document.createElement("p");
-
-            //   const resultItemData = document.createElement("div");
-            //   resultItemData.className = "data";
-            //   const resultItemDataTotal = document.createElement("p");
-            //   const resultItemDataAva = document.createElement("p");
-            //   const resultItemDataAll = document.createElement("p");
-            //   const resultItemDataFree = document.createElement("p");
-
-            //   if (freeSlots === 0) {
-            //     resultItemDataFree.className = "no_spot";
-            //   }
-
-            //   resultItemInfoName.innerText = stationName.replace("YouBike2.0_", "").replace("捷運", "");
-            //   resultItemInfoDistrict.innerText = district;
-            //   resultItemInfoAddress.innerText = address;
-            //   resultItemDataTotal.innerText = "Total";
-            //   resultItemDataAva.innerText = "Available";
-            //   resultItemDataAll.innerText = totalSpots;
-            //   resultItemDataFree.innerText = freeSlots;
-
-            //   resultContainer.appendChild(resultItem);
-            //   resultItem.appendChild(resultItemContent);
-            //   resultItemContent.append(
-            //     resultItemInfo,
-            //     resultItemData,
-            //     resultItemLink,
-            //   );
-            //   resultItemInfo.append(
-            //     resultItemInfoName,
-            //     resultItemInfoDistrict,
-            //     resultItemInfoAddress
-            //   );
-            //   resultItemData.append(
-            //     resultItemDataTotal,
-            //     resultItemDataAva,
-            //     resultItemDataAll,
-            //     resultItemDataFree
-            //   );
-            
-
-            // } 
-          // });
           // Q: how to get the location of browser?
-          // Q: how to make the JS code better and simpler?
           // Q: how to click btn again to clear the result first and then do the search?
-          // Q: is it possible to make the API call into a function itself, and then I call if when user click?
-        // }
-  //     });
-  //   } else {
-  //     alert("無法搜尋，搜尋關鍵字至少擇其一搜尋：行政區or地址關鍵字");
-  //   }
 
-  // });
 
-  // clear the search input ＆ search result
-  // clearbtn.addEventListener("click", (term) => {
-  //   searchInputDisc.value = "";
-  //   searchInputTxt.value = "";
-  //   resultContainer.innerHTML = "";
-  //   footer.className = "footer";
-  // });
-
-// });
